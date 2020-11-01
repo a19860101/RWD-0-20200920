741708c00fafebcc0472a29b8faca9ffe85ed99f
Zac <a19860101@gmail.com>
2020-11-01 16:10:17 +0800

    sabon

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
-    $t = glob('./*');
-    var_dump($t);
+    $path = glob('./*');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($path as $p){ ?>
+        <li><a href="/<?php echo $p ?>"><?php echo $p ?></a></li>
+        <?php } ?>
+    </ul>  
+</body>
+</html>
